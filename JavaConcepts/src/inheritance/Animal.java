57aa0cb01a9613401a15eff83e97a5ff851dcc60
Gayacp <gayaprabhakaran120@gmail.com>
2021-03-16 16:36:23 -0400

    commit from X after branch added

--- JavaConcepts/src/inheritance/Animal.java
+++ JavaConcepts/src/inheritance/Animal.java
@@ -11,6 +11,8 @@ public class Animal {
 		System.out.println("Generating sound2");
 		System.out.println("Generating sound3");
 		System.out.println("Generating sound4");
+		System.out.println("Generating sound5");
+		System.out.println("Generating sound6");
 		
 	}
 
