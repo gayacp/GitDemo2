87b50baa84b7e3ce031087cdb775c34dc57adbe8
Gayacp <gayaprabhakaran120@gmail.com>
2021-03-16 16:01:27 -0400

    commit from X

--- JavaConcepts/src/inheritance/Animal.java
+++ JavaConcepts/src/inheritance/Animal.java
@@ -9,6 +9,8 @@ public class Animal {
 		System.out.println("Generate sound");
 		System.out.println("Generating sound1");
 		System.out.println("Generating sound2");
+		System.out.println("Generating sound3");
+		System.out.println("Generating sound4");
 		
 	}
 
