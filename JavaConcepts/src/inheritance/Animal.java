acfc26758105aa67d173a3c1d86bc5185fcfd416
Gayacp <gayaprabhakaran120@gmail.com>
2021-03-16 15:29:05 -0400

    second commit

--- JavaConcepts/src/inheritance/Animal.java
+++ JavaConcepts/src/inheritance/Animal.java
@@ -7,6 +7,8 @@ public class Animal {
 	public void sound(){
 		
 		System.out.println("Generate sound");
+		System.out.println("Generating sound1");
+		System.out.println("Generating sound2");
 		
 	}
 
